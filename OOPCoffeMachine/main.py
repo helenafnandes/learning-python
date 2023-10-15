aa782fd471aefffe259ceb85483759d0f34c04a5
helena <helenaff13@gmail.com>
2023-10-14 22:34:21 -0300

    reorginazed files

--- OOPCoffeMachine/main.py
+++ OOPCoffeMachine/main.py
@@ -19,5 +19,6 @@ while is_on:
         money_machine.report()
     else:
         drink = menu.find_drink(answer)
-        if coffee_maker.is_resource_sufficient(drink) and money_machine.make_payment(drink.cost):
-            coffee_maker.make_coffee(drink)
+        if coffee_maker.is_resource_sufficient(drink):
+            if money_machine.make_payment(drink.cost):
+                coffee_maker.make_coffee(drink)
